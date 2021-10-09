f4707dc30560f19f7a752216f681790d37760605
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2021-10-09 14:59:06 +0000

    Added Program 16
    * Program 16 is all ready with some
      really awesome ASCII formatting.

--- 2021/October/program16.py
+++ 2021/October/program16.py
@@ -1,15 +1,15 @@
 # Copyright © 2021 Advaith Menon
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the “Software”), to deal in the
 # Software without restriction, including without limitation the rights to use, copy,
 # modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
 # and to permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -17,12 +17,10 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 """Sort a list"""
 
 import logging
 import typing as t
-
 
 logger = logging.getLogger(__name__)
 
@@ -38,8 +36,7 @@ def is_number(item: t.Any) -> bool:
     Returns:
         bool: Whether it's no or not
     """
-    if type(item) == int or type(item) == str:
-        print(type(item))
+    if type(item) == int or type(item) == float:
         return True
     else:
         return False
@@ -59,18 +56,18 @@ def bubble_sort(list_):
     lst = list_[:]
     logger.debug(f"list {lst}")
 
-    for pass_no in range(1, len(lst)+1):
+    for pass_no in range(1, len(lst) + 1):
         logger.debug(f"Pass no {pass_no}")
-        for i in range(len(lst)-pass_no):
+        for i in range(len(lst) - pass_no):
             logger.debug(f"i {i}")
             logger.debug(f"{lst[i]} and {lst[i+1]}")
 
-            if not (is_number(lst[i]) and is_number(lst[i+1])):
+            if not (is_number(lst[i]) and is_number(lst[i + 1])):
                 raise ValueError("Uncomparable Datatypes")
 
-            if lst[i]>lst[i+1]:
+            if lst[i] > lst[i + 1]:
                 logger.debug(f"switched")
-                lst[i], lst[i+1] = lst[i+1], lst[i]
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
                 logger.debug(f"after: {lst[i]} and {lst[i+1]}")
             logger.debug(f"After operation: {lst}")
             logger.debug(f"----------------------")
@@ -80,5 +77,29 @@ def bubble_sort(list_):
     return lst
 
 
-if __name__=="__main__":
-    logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+if __name__ == "__main__":
+    logging.basicConfig(
+      #encoding='utf-8', 
+      level=logging.INFO)
+
+    list_2_sort = list()
+
+    while True:
+        inp = input("Please enter a number. " "Type EXIT to exit: ")
+        if inp.upper() != "EXIT":
+            if inp.isdecimal():
+                list_2_sort.append(int(inp))
+            else:
+                print("Grr! I understand only numbers.")
+        else:
+            break
+
+    sorted_lst = bubble_sort(list_2_sort)
+
+    print("The sorted items are:")
+
+    for i in range(len(sorted_lst)):
+        if i == len(sorted_lst) - 1:
+            print(f"  |_ {i+1}.: {sorted_lst[i]}")
+        else:
+            print(f"  |- {i+1}.: {sorted_lst[i]}")
--- 2021/October/unittest_p16.py
+++ 2021/October/unittest_p16.py
@@ -5,8 +5,6 @@ import program16 as p
 
 class Program16TC(unittest.TestCase):
     def test_no(self):
-        lst = [random.randrange(1, 1000) for i in range(1, 100)]
-
         self.assertTrue(p.is_number(111))
         self.assertFalse(p.is_number("pigeon"))
         self.assertTrue(p.is_number(3.11))
@@ -14,7 +12,7 @@ class Program16TC(unittest.TestCase):
     def test_sort(self):
         lst = [random.randrange(1, 1000) for i in range(1, 100)]
 
-        self.assertEqual(lst.sort(), p.bubble_sort(lst))
+        self.assertEqual(sorted(lst), p.bubble_sort(lst))
 
 if __name__=="__main__":
     unittest.main(verbosity=2)
